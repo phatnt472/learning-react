5bdaa9c34e7a29d57af944a30b44ba05ab4c839f
phatnt472 <20521737@gm.uit.edu.vn>
2023-11-04 14:51:52 +0700

    fifth commit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <MyComponent />
+        <MyComponent>Abc</MyComponent>
       </header>
-    </div>
+    </div> 
   );
 }
 
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,8 +1,21 @@
 import React from "react";
 
-class MyComponent extends React.Component{
-    render()  {
-        return (<h1>Đây là 1 Component</h1>)
+class MyComponent extends React.Component {
+
+    state = {
+        name: ""
+    }
+    handleChangeInput = (event) => {
+        this.setState({name: event.target.value})
+    }
+    render() {
+        const { name } = this.state
+        return (
+            <>
+                <input type="text" value={name} onChange={(event) => this.handleChangeInput(event)}/>
+                <h1>Đây là 1 Component make by {name}</h1>
+            </>
+        )
     }
 }
 
