b5e7c5bf0a019875b09cce744c3be1549c7afe82
phatnt472 <20521737@gm.uit.edu.vn>
2023-11-09 08:58:38 +0700

    fifteenth commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import MyComponent from './components/MyComponent'
+
 
 function App() {
   return (
     <div className="App">
       <div className='App-header'>
         <img src={logo} className="App-logo" alt="logo" />
-        <MyComponent />
+        <h1 className='text-[50px] text-red-400'>Hi</h1>
       </div>
     </div>
   );
